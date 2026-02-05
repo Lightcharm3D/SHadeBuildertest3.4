8fee6876136ef61355f705ca9eb3c242900f50fc
[dyad] <git@dyad.sh>
2026-02-04 20:40:13 -0800

    [dyad] Modernizing Android theme and splash screen for a fresh start - wrote 5 file(s)

--- android/app/src/main/java/com/lightcharm/shadebuilder/MainActivity.java
+++ android/app/src/main/java/com/lightcharm/shadebuilder/MainActivity.java
@@ -1,5 +1,17 @@
 package com.lightcharm.shadebuilder;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        
+        // Optional: Make the app content appear behind system bars for a more immersive feel
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}

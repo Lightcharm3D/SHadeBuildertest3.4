fd6cd17447f9fd9e0b0ae0da736d693bceab2567
[dyad] <git@dyad.sh>
2026-02-05 12:20:38 -0800

    [dyad] Configuring the app for static web hosting - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Index from "./pages/Index";
 import LithophaneGenerator from "./pages/LithophaneGenerator";
@@ -28,13 +28,13 @@ const App = () => {
         </AnimatePresence>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/lithophane" element={<LithophaneGenerator />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
